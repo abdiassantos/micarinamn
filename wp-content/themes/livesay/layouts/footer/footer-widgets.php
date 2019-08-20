ae523f5c5e8ce17cabc91d27bf9a93fc96a197d7
abdviana@gmail.com <abdviana@gmail.com>
2019-08-20 14:24:47 -0300

    deleting files

--- wp-content/themes/livesay/layouts/footer/footer-widgets.php
+++
@@ -1,5 +0,0 @@
-<!-- Footer Widgets -->
-<div class="container">
-	<?php echo livesay_vt_footer_widgets(); ?>
-</div>
-<!-- Footer Widgets -->
--- wp-content/themes/livesay/searchform.php
+++
@@ -1,9 +0,0 @@
-<div class="lvsy-search-form">
-	<form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>" class="searchform" >
-		<div>
-			<label class="screen-reader-text" for="s"><?php esc_html_e( 'Search for:', 'livesay' ); ?></label>
-			<input type="text" name="s" id="s" placeholder="<?php esc_html_e('Search', 'livesay'); ?>" />
-			<input type="submit" id="searchsubmit" value="" />
-		</div>
-	</form>
-</div>
